1133ce6b3515844fabb3df9c637115a4c591e9fe
Gauransh9958 <gauransh9958@gmail.com>
2024-09-24 21:26:40 +0530

    Java

--- Patterns (PART-I)/CharPattern.java
+++ Patterns (PART-I)/CharPattern.java
@@ -1,3 +1,23 @@
+
+import java.util.*;
+
 public class CharPattern{
     public static void main(String[] args) {
-        
+Scanner sc = new Scanner(System.in);
+
+// print char pattern by taking char as input
+
+
+System.out.println("Enter the value of n : ");
+    int n =sc.nextInt();
+        char ch = 'A';
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+System.out.println();
+
+        }
+    }
+}
